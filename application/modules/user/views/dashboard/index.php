a03bb66f4bdcdf4cde07e2c2891ace8871a12afd
bryanstyawan <bryanstyawan@hotmail.com>
2019-01-29 15:47:46 +0700

    released update dashboard user

---
+++ application/modules/user/views/dashboard/index.php
@@ -0,0 +1,97 @@
+<div class="container">
+    <div class="col-lg-12">
+        <div class="row">
+            <div class="box box-solid">
+                <div class="box-body">
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class=""></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+                        </ol>
+                        <div class="carousel-inner">
+                        <div class="item active left">
+                            <img src="http://placehold.it/1500x500/39CCCC/ffffff&amp;text=Try+Out" alt="Try Out">
+
+                            <div class="carousel-caption">
+                            Try Out
+                            </div>
+                        </div>
+                        <div class="item next left">
+                            <img src="http://placehold.it/1500x500/3c8dbc/ffffff&amp;text=Bimbingan+Belajar" alt="Bimbingan Belajar">
+
+                            <div class="carousel-caption">
+                            Bimbingan Belajar
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="http://placehold.it/1500x500/f39c12/ffffff&amp;text=Video+Materi" alt="Video Materi">
+
+                            <div class="carousel-caption">
+                            Video Materi
+                            </div>
+                        </div>
+                        </div>
+                        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                        <span class="fa fa-angle-left"></span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                        <span class="fa fa-angle-right"></span>
+                        </a>
+                    </div>
+                </div>
+            </div>        
+        </div>
+        <div class="row">
+
+            <div class="row">
+                <a href="<?=base_url();?>user/try_out">
+                    <div class="col-md-6">
+                        <div class="box mb-4 shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Try "><title>Placeholder</title><rect fill="#39CCCC" width="100%" height="100%"></rect><text fill="#eceeef" dy=".22px" x="50%" y="50%">Try Out</text></svg>
+                            <div class="box-body">
+                                <p class="card-text">Try Out.</p>
+                            </div>
+                        </div>
+                    </div>                
+                </a>
+
+                <a href="<?=base_url();?>user/bimbingan_belajar">
+                    <div class="col-md-6">
+                        <div class="box mb-4 shadow-sm">
+                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect fill="#3c8dbc" width="100%" height="100%"></rect><text fill="#eceeef" dy=".3em" x="50%" y="50%">Bimbingan Belajar</text></svg>
+                            <div class="box-body">
+                                <p class="card-text">Bimbingan Belajar.</p>
+                            </div>
+                        </div>
+                    </div>                
+                </a>
+            </div>
+            <div class="row">
+
+                <a href="<?=base_url();?>user/video_materi">            
+                    <div class="col-md-6">
+                        <div class="box mb-4 shadow-sm">
+                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect fill="#f39c12" width="100%" height="100%"></rect><text fill="#eceeef" dy=".3em" x="50%" y="50%">Video Materi</text></svg>
+                            <div class="box-body">
+                                <p class="card-text">Video Materi.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="<?=base_url();?>dashboard/soon">
+                    <div class="col-md-6">
+                        <div class="box mb-4 shadow-sm">
+                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect fill="#55595c" width="100%" height="100%"></rect><text fill="#eceeef" dy=".3em" x="50%" y="50%">Mini Try Out</text></svg>
+                            <div class="box-body">
+                                <p class="card-text">Mini Try Out.</p>
+                            </div>
+                        </div>
+                    </div>        
+                </a>
+            </div>            
+
+        </div>
+    </div>
+</div>
